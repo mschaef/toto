105b559d01e0a1f0bbc443e3d484f9a97b669dbc
mschaef <schaeffer.michael.a@gmail.com>
2012-01-15 22:11:19 -0500

    Initial commit.

---
+++ src/main/java/com/ectworks/toto/domain/TodoItem.java
@@ -0,0 +1,16 @@
+package com.ectworks.toto.domain;
+
+public class TodoItem
+{
+    String description;
+
+    void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    String getDescription()
+    {
+        return description;
+    }
+}
