cbba5bdabd5a6a9dd7c5e308aa5a3cb9798a770b
mschaef <schaeffer.michael.a@gmail.com>
2013-03-29 20:48:29 -0400

    Move schema defintion into Clojure

--- setup-db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-./libs/h2/bin/h2-shell.sh -url jdbc:h2:~/ectworks/toto/toto.h2db -user sa \
-  < ./src/main/resources/schema.sql
--- src/toto/data.clj
+++ src/toto/data.clj
@@ -3,7 +3,7 @@
             [clojure.java.jdbc :as sql]))
 
 (def hsql-db {:subprotocol "hsqldb"
-              :subname "file:~/ectworks/toto/toto.h2db"
+              :subname "~/ectworks/toto/toto.h2db"
               :user "sa"
               :password ""
               })
@@ -14,3 +14,25 @@
       ["select table_name from information_schema.tables order by table_name"]
       (doall (map :table_name rows)))))
 
+
+(defn setup-schema []
+ (sql/with-connection hsql-db
+   (sql/create-table
+    :user
+    [:user_id "BIGINT" "identity"]
+    [:name "varchar(255)" "unique"]
+    [:password "varchar(255)"]
+    [:email_addr "varchar(255)"])
+
+   (sql/create-table
+    :todo_item
+    [:item_id "BIGINT" "identity"]
+    [:user_id "BIGINT" "references user(user_id)"]
+    [:desc "varchar(255)"]
+    [:completed "boolean"])
+   
+   (sql/insert-records
+    :user
+    {:name "mschaef"
+     :password "14d5b8f25f499c041a12508a9be7b87e52db818e3a06bf6fe970a7fe7d39a1e5"
+     :email_addr "schaeffer.michael.a@gmail.com"})))
