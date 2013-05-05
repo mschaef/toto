0fc68c325918a964709037ee1b2d4bb5440fe173
mschaef <schaeffer.michael.a@gmail.com>
2013-05-05 17:12:20 -0400

    Add separate schema definiton file.

--- src/toto/data.clj
+++ src/toto/data.clj
@@ -7,12 +7,6 @@
               :password ""
               })
 
-(defn all-table-names []
-  (sql/with-connection hsql-db
-    (sql/with-query-results rows
-      ["select table_name from information_schema.tables order by table_name"]
-      (doall (map :table_name rows)))))
-
 (defn all-user-names []
   (sql/with-connection hsql-db
     (sql/with-query-results rows
@@ -24,28 +18,6 @@
     (sql/with-query-results rows
       ["select * from information_schema.tables where table_name=?" table-name]
       (doall rows))))
-
-(defn setup-schema []
- (sql/with-connection hsql-db
-   (sql/create-table
-    :user
-    [:user_id "BIGINT" "identity"]
-    [:name "varchar(255)" "unique"]
-    [:password "varchar(255)"]
-    [:email_addr "varchar(255)"])
-
-   (sql/create-table
-    :todo_item
-    [:item_id "BIGINT" "identity"]
-    [:user_id "BIGINT" "references user(user_id)"]
-    [:desc "varchar(255)"]
-    [:completed "boolean"])
-   
-   (sql/insert-records
-    :user
-    {:name "mschaef"
-     :password "14d5b8f25f499c041a12508a9be7b87e52db818e3a06bf6fe970a7fe7d39a1e5"
-     :email_addr "schaeffer.michael.a@gmail.com"})))
 
 (defn add-user [name password email-addr]
   (sql/with-connection hsql-db
@@ -90,9 +62,4 @@
      [ "item_id=?" item-id]
      { :completed true })))
 
-;; (defn remove-item-by-id [ item-id ]
-;;   (sql/with-connection hsql-db
-;;     (sql/delete
-;;      :todo_item
-;;      [ "item_id=?" item-id])))
-
+;; TODO: remove-item-by-id
---
+++ src/toto/schema.clj
@@ -0,0 +1,40 @@
+(ns toto.schema
+  (:require [clojure.java.jdbc :as sql]))
+
+(defn all-table-names []
+  (sql/with-connection hsql-db
+    (sql/with-query-results rows
+      ["select table_name from information_schema.tables order by table_name"]
+      (doall (map :table_name rows)))))
+
+(defn setup-schema []
+ (sql/with-connection hsql-db
+
+   (sql/create-table
+    :toto_schema_version
+    [:version_number "BIGINT"])
+
+   (sql/insert-records
+    :schema_version
+    {:version_number 1})
+   
+   (sql/create-table
+    :user
+    [:user_id "BIGINT" "identity"]
+    [:name "varchar(255)" "unique"]
+    [:password "varchar(255)"]
+    [:email_addr "varchar(255)"])
+
+   (sql/create-table
+    :todo_item
+    [:item_id "BIGINT" "identity"]
+    [:user_id "BIGINT" "references user(user_id)"]
+    [:desc "varchar(255)"]
+    [:completed "boolean"])
+   
+   (sql/insert-records
+    :user
+    {:name "mschaef"
+     :password "14d5b8f25f499c041a12508a9be7b87e52db818e3a06bf6fe970a7fe7d39a1e5"
+     :email_addr "schaeffer.michael.a@gmail.com"})))
+
