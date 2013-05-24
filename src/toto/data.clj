bda0dd577ca330cab657be68f83dbcd2b1e494aa
mschaef <schaeffer.michael.a@gmail.com>
2013-05-23 22:16:24 -0400

    Switch to jdbc as a name for clojure.java.jdbc.

--- src/toto/data.clj
+++ src/toto/data.clj
@@ -1,59 +1,59 @@
 (ns toto.data
-  (:require [clojure.java.jdbc :as sql]
+  (:require [clojure.java.jdbc :as jdbc]
             [toto.schema :as schema]))
 
 
 (defn all-user-names []
-  (sql/with-connection schema/hsql-db
-    (sql/with-query-results rows
+  (jdbc/with-connection schema/hsql-db
+    (jdbc/with-query-results rows
       ["select name from user order by name"]
       (doall (map :name rows)))))
 
 (defn table-info [table-name]
-  (sql/with-connection schema/hsql-db
-    (sql/with-query-results rows
+  (jdbc/with-connection schema/hsql-db
+    (jdbc/with-query-results rows
       ["select * from information_schema.tables where table_name=?" table-name]
       (doall rows))))
 
 (defn add-user [name password email-addr]
-  (sql/with-connection schema/hsql-db
+  (jdbc/with-connection schema/hsql-db
     (:user_id (first
-               (sql/insert-records
+               (jdbc/insert-records
                 :user
                 {:name name
                  :password password
                  :email_addr email-addr})))))
 
 (defn get-user-by-name [ user-name ]
-  (sql/with-connection schema/hsql-db
-    (sql/with-query-results rows
+  (jdbc/with-connection schema/hsql-db
+    (jdbc/with-query-results rows
       ["select * from user where name=?" user-name]
       (first rows))))
 
 (defn add-todo-item [ user-id desc ]
-  (sql/with-connection schema/hsql-db
+  (jdbc/with-connection schema/hsql-db
     (:item_id (first
-               (sql/insert-records
+               (jdbc/insert-records
                 :todo_item
                 {:user_id user-id
                  :desc desc
                  :completed false})))))
 
 (defn get-pending-items [ ]
-  (sql/with-connection schema/hsql-db
-    (sql/with-query-results rows
+  (jdbc/with-connection schema/hsql-db
+    (jdbc/with-query-results rows
       ["select user_id, desc from todo_item where completed=false" ]
       (doall rows))))
 
 (defn get-item-by-id [ item-id ]
-  (sql/with-connection schema/hsql-db
-    (sql/with-query-results rows
+  (jdbc/with-connection schema/hsql-db
+    (jdbc/with-query-results rows
       ["select * from todo_item where item_id=?" item-id ]
       (doall rows))))
 
 (defn complete-item-by-id [ item-id ]
-  (sql/with-connection schema/hsql-db
-    (sql/update-values
+  (jdbc/with-connection schema/hsql-db
+    (jdbc/update-values
      :todo_item
      [ "item_id=?" item-id]
      { :completed true })))
--- src/toto/schema.clj
+++ src/toto/schema.clj
@@ -1,5 +1,5 @@
 (ns toto.schema
-  (:require [clojure.java.jdbc :as sql]))
+  (:require [clojure.java.jdbc :as jdbc]))
 
 (def hsql-db {:subprotocol "hsqldb"
               :subname "~/ectworks/toto/toto.h2db"
@@ -8,37 +8,37 @@
               })
 
 (defn all-table-names []
-  (sql/with-connection hsql-db
-    (sql/with-query-results rows
+  (jdbc/with-connection hsql-db
+    (jdbc/with-query-results rows
       ["select table_name from information_schema.tables order by table_name"]
       (doall (map :table_name rows)))))
 
 (defn setup-schema []
- (sql/with-connection hsql-db
+ (jdbc/with-connection hsql-db
 
-   (sql/create-table
+   (jdbc/create-table
     :toto_schema_version
     [:version_number "BIGINT"])
 
-   (sql/insert-records
+   (jdbc/insert-records
     :schema_version
     {:version_number 1})
    
-   (sql/create-table
+   (jdbc/create-table
     :user
     [:user_id "BIGINT" "identity"]
     [:name "varchar(255)" "unique"]
     [:password "varchar(255)"]
     [:email_addr "varchar(255)"])
 
-   (sql/create-table
+   (jdbc/create-table
     :todo_item
     [:item_id "BIGINT" "identity"]
     [:user_id "BIGINT" "references user(user_id)"]
     [:desc "varchar(255)"]
     [:completed "boolean"])
    
-   (sql/insert-records
+   (jdbc/insert-records
     :user
     {:name "mschaef"
      :password "14d5b8f25f499c041a12508a9be7b87e52db818e3a06bf6fe970a7fe7d39a1e5"
