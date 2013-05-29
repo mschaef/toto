6d70ce27086103edf4056947baae6cb706c6fc2e
mschaef <schaeffer.michael.a@gmail.com>
2013-05-28 22:36:55 -0400

    Add the current user identity to the footer of web pages.

--- src/toto/handler.clj
+++ src/toto/handler.clj
@@ -1,7 +1,8 @@
 (ns toto.handler
-  (:use toto.core)
+  (:use toto.view)
   (:use compojure.core)
   (:require [toto.data :as data]
+            [toto.core :as core]
             [compojure.route :as route]
             [compojure.handler :as handler]
             [cemerick.friend :as friend]
@@ -10,23 +11,24 @@
 
 (defroutes site-routes
   (GET "/" [] (friend/authenticated (render-todo-list)))
+
   (GET "/login" [] (render-login-page))
+
   (friend/logout (ANY "/logout" request (ring.util.response/redirect "/")))
 
   (POST "/item" {{item-description :item-description} :params}
         (add-item item-description))
+
   (GET "/users" [] (render-users))
   (GET "/user/:name" [name] (render-user name))
 
   (route/not-found "<h1>Page not found</h1>"))
 
-(def ^:dynamic *username* nil)
-
 (defn wrap-username [app]
   (fn [req]
-    (binding [*username* (if-let [cauth (friend/current-authentication req)]
-                           (cauth :username)
-                           nil)]
+    (binding [core/*username* (if-let [cauth (friend/current-authentication req)]
+                                (cauth :identity)
+                                nil)]
       (app req))))
 
 (defn db-credential-fn [ creds ]
---
+++ src/toto/view.clj
@@ -0,0 +1,59 @@
+(ns toto.view
+  (:use hiccup.core)
+  (:use clojure.set)
+  (:require [toto.data :as data]
+            [toto.core :as core]
+            [hiccup.form :as form]
+            [ring.util.response :as ring]))
+
+(def page-title "Toto")
+
+(defn render-page [& contents]
+  (html [:html
+         [:title page-title]
+         [:body contents]
+         [:hr]
+         [:a { :href "/logout"} "logout"]
+         (if (not (nil? core/*username*))
+           (str " - " core/*username*)
+           "")]))
+
+(defn render-login-page []
+  (render-page (form/form-to
+                [:post "/login"]
+                [:table
+                 [:tr [:td { :colspan 2 } [:center "Login to Toto"]]]
+                 [:tr [:td "Username:"] [:td (form/text-field {} "username")]]
+                 [:tr [:td "Password:"] [:td (form/password-field {} "password")]]
+                 [:tr [:td ] [:td (form/submit-button {} "Login")]]])))
+
+(defn add-item [item-description]
+  (data/add-todo-item 0 item-description)
+  (ring/redirect "/"))
+
+(defn render-todo-list []
+  (render-page [:table
+                [:tr [:td "User"] [:td "Description"]]
+                (map (fn [item-info]
+                       [:tr
+                        [:td (item-info :user_id)]
+                        [:td (item-info :desc)]])
+                     (data/get-pending-items))
+                [:tr [:td]
+                 [:td (form/form-to [:post "/item"]
+                                    (form/text-field {} "item-description"))]]]))
+
+(defn render-users []
+  (render-page [:h1 "List of Users"]
+               [:ul
+                (map (fn [user-name]
+                       [:li [:a {:href (str "/user/" user-name)} user-name]])
+                     (data/all-user-names))]))
+
+(defn render-user [name]
+  (let [user-info (data/get-user-by-name name)]
+    (render-page [:h1 (str "User: " (user-info :name))]
+                 [:table
+                  [:tr [:td "Name"] [:td (user-info :name)]]
+                  [:tr [:td "e-mail"] [:td (user-info :email_addr)]]
+                  [:tr [:td "password"] [:td (user-info :password)]]])))
