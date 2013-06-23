04638ee9aa84385b4bf9e3a15f8b77cf0714f0f3
mschaef <schaeffer.michael.a@gmail.com>
2013-06-23 16:28:16 -0400

    Refactor the toto package to prepare for rendering dashboards.

--- src/toto/todo.clj
+++ src/toto/todo.clj
@@ -14,35 +14,42 @@
 (defn current-todo-list-id []
   (first (data/get-todo-list-ids-by-user (current-user-id))))
 
+(defn redirect-to-home []
+  (ring/redirect (str "/todo/" (current-todo-list-id))))
+
 (defn complete-item-button [item-info]
   (form/form-to [:post (str "/item/" (item-info :item_id) "/complete")]
                 (form/submit-button {} "Complete")))
 
 (defn render-todo-list [ list-id ]
-  (view/render-page [:table
-                     [:tr [:td "User"] [:td "Description"] [:td]]
-                     (map (fn [item-info]
-                            [:tr
-                             [:td (item-info :todo_list_id)]
-                             [:td [:a {:href (str "/item/" (item-info :item_id))} (item-info :desc)]]
-                             [:td (complete-item-button item-info)]])
-                          (data/get-pending-items list-id))
-                     [:tr [:td]
-                      [:td (form/form-to [:post "/item"]
-                                         (form/text-field {} "item-description"))]]]))
+  [:table
+   [:tr [:td "User"] [:td "Description"] [:td]]
+   (map (fn [item-info]
+          [:tr
+           [:td (item-info :todo_list_id)]
+           [:td [:a {:href (str "/item/" (item-info :item_id))} (item-info :desc)]]
+           [:td (complete-item-button item-info)]])
+        (data/get-pending-items list-id))
+   [:tr [:td]
+    [:td (form/form-to [:post "/item"]
+                       (form/text-field {} "item-description"))]]])
+
+(defn render-todo-list-page [ list-id ]
+  (view/render-page
+   (render-todo-list list-id)))
 
 (defn add-item [item-description]
   (data/add-todo-item (current-todo-list-id)
                       item-description)
-  (ring/redirect "/"))
+  (redirect-to-home))
 
 (defn update-item [item-id item-description]
   (data/update-item-by-id item-id item-description)
-  (ring/redirect "/"))
+  (redirect-to-home))
 
 (defn complete-item [item-id]
   (data/complete-item-by-id item-id)
-  (ring/redirect "/"))
+  (redirect-to-home))
 
 (defn render-item [id]
   (let [item-info (data/get-item-by-id id)]
@@ -56,10 +63,10 @@
 
 (defroutes all-routes
   (GET "/" []
-       (ring/redirect (str "/todo/" (current-todo-list-id))))
+       (redirect-to-home))
 
   (GET "/todo/:list-id" [ list-id ]
-       (render-todo-list list-id))
+       (render-todo-list-page list-id))
 
   (POST "/item" {{item-description :item-description} :params}
         (add-item item-description))
