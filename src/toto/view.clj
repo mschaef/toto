3f613805151cb1067cbe871b97f3222454fdfc84
Mike Schaeffer <mschaeffer@ksmpartners.com>
2013-09-13 14:52:47 -0400

    Add home link at top left of each page.

--- src/toto/view.clj
+++ src/toto/view.clj
@@ -8,33 +8,32 @@
 
 (def page-title "Toto")
 
-(defn render-page [{ :keys [ page-title ] } 
-                   &
-                   contents]
-  (let [ full-page-title (if (nil? page-title)
-                           "Toto"
-                           (str "Toto - " page-title))]
-    (html [:html
-           [:head
-            [:title full-page-title]       
-            (page/include-css "/reset.css")
-            (page/include-css "/toto.css")
-            (page/include-js "/jquery-1.10.1.js")
-            (page/include-js "/toto.js")
-            [:link { :rel "shortcut icon" :href "/favicon.ico"}]]
-           [:body
-            [:div#header 
-             full-page-title
-             [:div.right
-              (if-let [un (core/authenticated-username)]
-                [:span 
-                 (str un)
-                 " - "
-                 [:span#logout
-                  [:a { :href "/logout"} "[logout]"]]])]]
-            [:div#wrap
-             contents
-             [:div#footer
-              "All Rights Reserved, Copyright 2013 East Coast Toolworks"]]]])))
+(defn render-page [{ :keys [ page-title ] } & contents]
+  (html [:html
+         [:head
+          [:title (if (nil? page-title)
+                    "Toto"
+                    (str "Toto - " page-title))]
+          (page/include-css "/reset.css")
+          (page/include-css "/toto.css")
+          (page/include-js "/jquery-1.10.1.js")
+          (page/include-js "/toto.js")
+          [:link { :rel "shortcut icon" :href "/favicon.ico"}]]
+         [:body
+          [:div#header 
+           [:a { :href "/" } "Toto"]
+           (if (not (nil? page-title))
+             (str " - " page-title))
+           [:div.right
+            (if-let [un (core/authenticated-username)]
+              [:span 
+               (str un)
+               " - "
+               [:span#logout
+                [:a { :href "/logout"} "[logout]"]]])]]
+          [:div#wrap
+           contents
+           [:div#footer
+            "All Rights Reserved, Copyright 2013 East Coast Toolworks"]]]]))
 
 
