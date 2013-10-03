d8f460129ac83b2574e88ebb12c01ed76fbbac6c
Mike Schaeffer <mschaeffer@ksmpartners.com>
2013-10-03 12:18:23 -0400

    Add rendering time to the bottom of each page.

--- src/toto/view.clj
+++ src/toto/view.clj
@@ -9,32 +9,35 @@
 (def page-title "Toto")
 
 (defn render-page [{ :keys [ page-title ] } & contents]
-  (html [:html
-         [:head
-          [:title (if (nil? page-title)
-                    "Toto"
-                    (str "Toto - " page-title))]
-          (page/include-css "/reset.css")
-          (page/include-css "/toto.css")
-          (page/include-js "/jquery-1.10.1.js")
-          (page/include-js "/jquery-ui.js")
-          (page/include-js "/toto.js")
-          [:link { :rel "shortcut icon" :href "/favicon.ico"}]]
-         [:body
-          [:div#header 
-           [:a { :href "/" } "Toto"]
-           (if (not (nil? page-title))
-             (str " - " page-title))
-           [:div.right
-            (if-let [un (core/authenticated-username)]
-              [:span 
-               (str un)
-               " - "
-               [:span#logout
-                [:a { :href "/logout"} "[logout]"]]])]]
-          [:div#wrap
-           contents
-           [:div#footer
-            "All Rights Reserved, Copyright 2013 East Coast Toolworks"]]]]))
+  (let [ t-begin (. System (nanoTime))]
+    (html [:html
+           [:head
+            [:title (if (nil? page-title)
+                      "Toto"
+                      (str "Toto - " page-title))]
+            (page/include-css "/reset.css")
+            (page/include-css "/toto.css")
+            (page/include-js "/jquery-1.10.1.js")
+            (page/include-js "/jquery-ui.js")
+            (page/include-js "/toto.js")
+            [:link { :rel "shortcut icon" :href "/favicon.ico"}]]
+           [:body
+            [:div#header 
+             [:a { :href "/" } "Toto"]
+             (if (not (nil? page-title))
+               (str " - " page-title))
+             [:div.right
+              (if-let [un (core/authenticated-username)]
+                [:span 
+                 (str un)
+                 " - "
+                 [:span#logout
+                  [:a { :href "/logout"} "[logout]"]]])]]
+            [:div#wrap
+             contents
+             [:div#footer
+              "All Rights Reserved, Copyright 2013 East Coast Toolworks "
+              (format "(%.1f msec.)" (/ (- (. System (nanoTime)) t-begin) 1000000.0))
+              ]]]])))
 
 
