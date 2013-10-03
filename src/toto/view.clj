0217e47c1f5939744d68d935ebb4f7f9b474d673
Mike Schaeffer <mschaeffer@ksmpartners.com>
2013-10-03 16:53:14 -0400

    Simplify the top-level rendering of pages.

--- src/toto/view.clj
+++ src/toto/view.clj
@@ -6,28 +6,25 @@
             [hiccup.form :as form]
             [hiccup.page :as page]))
 
-(def page-title "Toto")
+(def app-name "Toto")
 
 (defn render-page [{ :keys [ page-title include-js sidebar ] }  & contents]
   (let [ t-begin (. System (nanoTime))]
     (html [:html
            [:head
-            [:title (if (nil? page-title)
-                      "Toto"
-                      (str "Toto - " page-title))]
+            [:title app-name (if (not (nil? page-title)) (str " - " page-title))]
+            [:link { :rel "shortcut icon" :href "/favicon.ico"}]
+
             (page/include-css "/reset.css")
             (page/include-css "/toto.css")
             (page/include-js "/jquery-1.10.1.js")
             (page/include-js "/jquery-ui.js")
             (page/include-js "/toto.js")
-            (map #(page/include-js %) include-js)
+            (map #(page/include-js %) include-js)]
 
-            [:link { :rel "shortcut icon" :href "/favicon.ico"}]]
            [:body
             [:div#header 
-             [:a { :href "/" } "Toto"]
-             (if (not (nil? page-title))
-               (str " - " page-title))
+             [:a { :href "/" } app-name] (if (not (nil? page-title)) (str " - " page-title))
              [:div.right
               (if-let [un (core/authenticated-username)]
                 [:span 
@@ -41,7 +38,6 @@
                contents)
              [:div#footer
               "All Rights Reserved, Copyright 2013 East Coast Toolworks "
-              (format "(%.1f msec.)" (/ (- (. System (nanoTime)) t-begin) 1000000.0))
-              ]]]])))
+              (format "(%.1f msec.)" (/ (- (. System (nanoTime)) t-begin) 1000000.0))]]]])))
 
 
