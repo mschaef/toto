3da8b650b07210eecf919382592db1a327cad11a
Mike Schaeffer <mschaeffer@ksmpartners.com>
2013-10-26 22:39:48 -0400

    Correct padding at top of wrap div.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject toto "0.2.1b-SNAPSHOT"
+(defproject toto "0.2.1c-SNAPSHOT"
   :description "Toto To-Do List Manager"
   :license { :name "Copyright East Coast Toolworks (c) 2012-2013"}
 
