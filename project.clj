f397520d7e4fb44a5dd6ff1628a75f37efc397bb
Mike Schaeffer <mschaeffer@ksmpartners.com>
2013-11-01 21:43:14 -0400

    Upgrade dependencies.

--- project.clj
+++ project.clj
@@ -2,19 +2,19 @@
   :description "Toto To-Do List Manager"
   :license { :name "Copyright East Coast Toolworks (c) 2012-2013"}
 
-  :plugins [[lein-ring "0.8.5"]]
+  :plugins [[lein-ring "0.8.7"]]
 
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/java.jdbc "0.2.3"]
                  [org.clojure/tools.logging "0.2.6"]
                  [log4j/log4j "1.2.16"]
                  [org.hsqldb/hsqldb "2.3.0"]
                  [clj-http "0.6.4"]
-                 [hiccup "1.0.1"]
-                 [ring/ring-servlet "1.2.0-beta2"]
-                 [com.cemerick/friend "0.1.5"]
-                 [compojure "1.1.5"]
-                 [slingshot "0.10.2"]]
+                 [hiccup "1.0.4"]
+                 [ring/ring-servlet "1.2.0"]
+                 [com.cemerick/friend "0.2.0"]
+                 [compojure "1.1.6"]
+                 [slingshot "0.10.3"]]
 
   :ring {
          :init toto.schema/ensure-schema-available
