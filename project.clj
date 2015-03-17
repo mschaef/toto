99ca238e9b2ce20ed38729ec27cbae28c8105d0b
mschaef <schaeffer.michael.a@gmail.com>
2015-03-17 19:50:07 -0400

    Add lein-ancient and update a few deps.

--- project.clj
+++ project.clj
@@ -2,19 +2,20 @@
   :description "Toto To-Do List Manager"
   :license { :name "Copyright East Coast Toolworks (c) 2012-2015"}
 
-  :plugins [[lein-ring "0.9.0"]]
+  :plugins [[lein-ring "0.9.0"]
+            [lein-ancient "0.6.5"]]
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.0.9"]
+                 [ch.qos.logback/logback-classic "1.1.2"]
                  [com.ksmpartners/sql-file "0.1.0"]
                  [clj-http "1.0.1"
                   :exclusions [com.fasterxml.jackson.core/jackson-core]]
                  [hiccup "1.0.5"]
                  [ring/ring-jetty-adapter "1.3.2"]
                  [com.cemerick/friend "0.2.1"]
-                 [compojure "1.3.1"]
-                 [slingshot "0.12.1"]]
+                 [compojure "1.3.2"]
+                 [slingshot "0.12.2"]]
 
   :main toto.main
 
@@ -25,5 +26,4 @@
          }
 
   :jar-name "toto.jar"
-  :uberjar-name "toto-standalone.jar"
-  )
+  :uberjar-name "toto-standalone.jar")
