b5c10bfdd6da8e01bcef2f5fe8f76c4f38b97dc7
Mike Schaeffer <michael.schaeffer@lab49.com>
2016-01-08 14:00:28 -0500

    Update dpeendencies.

--- project.clj
+++ project.clj
@@ -2,16 +2,17 @@
   :description "Toto To-Do List Manager"
   :license { :name "Copyright East Coast Toolworks (c) 2012-2015"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.2"]
+                 [ch.qos.logback/logback-classic "1.1.3"]
                  [com.ksmpartners/sql-file "0.1.0"]
-                 [clj-http "1.0.1"
-                  :exclusions [com.fasterxml.jackson.core/jackson-core]]
+                 [clj-http "2.0.0"
+                  ;:exclusions [com.fasterxml.jackson.core/jackson-core]
+                  ]
                  [hiccup "1.0.5"]
-                 [ring/ring-jetty-adapter "1.3.2"]
+                 [ring/ring-jetty-adapter "1.4.0"]
                  [com.cemerick/friend "0.2.1"]
-                 [compojure "1.3.2"]
+                 [compojure "1.4.0"]
                  [slingshot "0.12.2"]]
 
   :main toto.main
