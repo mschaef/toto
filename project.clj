258b2526951b2116ec05e1f0b7d649a0e0d54789
Mike Schaeffer <mike@mschaef.com>
2016-05-05 17:29:03 -0400

    Upgrade deps.

--- project.clj
+++ project.clj
@@ -2,17 +2,17 @@
   :description "Toto To-Do List Manager"
   :license { :name "Copyright East Coast Toolworks (c) 2012-2016"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.3"]
-                 [com.ksmpartners/sql-file "0.1.0"]
+                 [ch.qos.logback/logback-classic "1.1.7"]
+                 [sql-file "0.2.0-pre0"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-http "2.0.0"]
+                 [clj-http "3.0.1"]
                  [hiccup "1.0.5"]
                  [ring/ring-jetty-adapter "1.4.0"]
                  [slester/ring-browser-caching "0.1.1"]
                  [com.cemerick/friend "0.2.1"]
-                 [compojure "1.4.0"]
+                 [compojure "1.5.0"]
                  [slingshot "0.12.2"]]
 
   :main toto.main
--- src/toto/data.clj
+++ src/toto/data.clj
@@ -4,7 +4,9 @@
             [sql-file.core :as sql-file]))
 
 (def db-connection
-  (delay (sql-file/open-hsqldb-file-conn (config-property "db.subname" "toto-db") "toto" 0)))
+  (delay (sql-file/open-sql-file
+          (sql-file/hsqldb-file-conn (config-property "db.subname" "toto-db"))
+          [ "toto" 0 ])))
 
 
 (def ^:dynamic *db* nil)
