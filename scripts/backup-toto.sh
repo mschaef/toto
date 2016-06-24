4e0b7a3969690fa597a8a0c49a8d1aea9b3431fb
Mike Schaeffer <mike@mschaef.com>
2016-06-24 16:08:34 -0400

    Switch to standard package/install scheme.

--- scripts/backup-toto.sh
+++ scripts/backup-toto.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-(cd /usr/share/tomcat7/ectworks/toto/ && \
-  tar czvf ~/toto-backups/toto-backup-`date +%Y%m%d`.tgz toto.h2db.lck toto.h2db.log toto.h2db.properties toto.h2db.script)
+(cd db && \
+  tar czvf ~/toto-backups/toto-backup-`date +%Y%m%d`.tgz \
+     toto.h2db.lck toto.h2db.log toto.h2db.properties toto.h2db.script toto.h2db.data)
 
-s3cmd sync /home/mschaef/toto-backups s3://dunitall.com
+s3cmd sync /home/toto/toto-backups s3://dunitall.com
+
 
---
+++ toto-install/install.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+TOTO_USERNAME=toto
+
+# Must be root
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+# create user and group
+
+egrep "^${TOTO_USERNAME}" /etc/passwd >/dev/null
+
+if [ $? -eq 0 ]; then
+    echo "User exists: ${TOTO_USERNAME}"
+else
+    echo "Creating user: ${TOTO_USERNAME}"
+    
+    useradd --user-group --system ${TOTO_USERNAME}
+
+    if [ $? -ne 0 ]; then
+        echo "Could not create user: ${TOTO_USERNAME}"
+        exit 1
+    fi
+fi
+
+# Install jar in /usr/share
+
+install -v --group=root --owner=root --directory /usr/share/toto
+install -v --group=root --owner=root toto-standalone.jar /usr/share/toto
+
+# create log directory
+
+install -v --group=toto --owner=toto --directory /var/log/toto
+
+# create data directory
+
+install -v --group=toto --owner=toto --directory /var/lib/toto
+
+# Configuration Files
+
+install -v --group=root --owner=root --directory /etc/toto
+install -v --group=root --owner=root logback.xml /etc/toto
+
+# toto service configuration
+
+install -v --group=root --owner=root toto /etc/init.d
+
+update-rc.d toto defaults
+
