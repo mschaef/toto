227a4f4822989405239eba7e3b1ea72f5a038648
Mike Schaeffer <mike@mschaef.com>
2017-08-20 12:39:10 -0400

    Update dependencies.

--- project.clj
+++ project.clj
@@ -3,17 +3,17 @@
   :license { :name "Copyright East Coast Toolworks (c) 2012-2017"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 [ch.qos.logback/logback-classic "1.1.7"]
-                 [sql-file "0.2.0-pre0"]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [sql-file "0.2.0-pre1"]
                  [yesql "0.5.3"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-http "3.0.1"]
+                 [clj-http "3.7.0"]
                  [hiccup "1.0.5"]
-                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-jetty-adapter "1.6.2"]
                  [slester/ring-browser-caching "0.1.1"]
-                 [com.cemerick/friend "0.2.1"]
-                 [compojure "1.5.0"]
+                 [com.cemerick/friend "0.2.3"]
+                 [compojure "1.6.0"]
                  [slingshot "0.12.2"]]
 
   :main toto.main
