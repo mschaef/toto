c7f04ee7dd823f2c619322016ad99bb28e2bc5f6
Mike Schaeffer <mike@mschaef.com>
2022-01-09 09:18:30 -0500

    Remove a copyright notice from queries.sql that confuses yesql.

--- src/toto/data/queries.sql
+++ src/toto/data/queries.sql
@@ -1,24 +1,3 @@
--- Copyright (c) 2015-2022 Michael Schaeffer (dba East Coast Toolworks)
---
--- Licensed as below.
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---       http://www.apache.org/licenses/LICENSE-2.0
---
--- The license is also includes at the root of the project in the file
--- LICENSE.
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
---
--- You must not remove this notice, or any other, from this software.
-
 -- name: get-user-by-email
 SELECT user.*, count(login_failure.failed_on) AS login_failure_count
     FROM user LEFT JOIN login_failure ON user.user_id = login_failure.user_id
