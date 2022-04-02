e74c0a734281256cc3c7fc715e5dcd52d30c073e
Mike Schaeffer <mike@mschaef.com>
2022-04-02 16:47:31 -0400

    Modify the h2-local script to run without a global sqltool.rc file.

--- h2-local.sh
+++ h2-local.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rlwrap java -cp ./hsqldb/hsqldb.jar:./hsqldb/sqltool.jar  org.hsqldb.cmdline.SqlTool local "$@"
+rlwrap java -cp ./hsqldb/hsqldb.jar:./hsqldb/sqltool.jar  org.hsqldb.cmdline.SqlTool --rcfile=./sqltool.rc local "$@"
