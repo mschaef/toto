a275a0fd6cea52a24e530aef8b3cf35288bfbec4
Mike Schaeffer <mike@mschaef.com>
2024-07-08 21:36:31 -0400

    Add support for automatically archiving backup files to S3.

--- src/base/backup.clj
+++ src/base/backup.clj
@@ -27,6 +27,24 @@
             [base.scheduler :as scheduler]
             [playbook.config :as config]))
 
+(defn- s3-client [ archive-config ]
+  (-> (software.amazon.awssdk.services.s3.S3Client/builder)
+      (.credentialsProvider
+       (software.amazon.awssdk.auth.credentials.StaticCredentialsProvider/create
+        (software.amazon.awssdk.auth.credentials.AwsBasicCredentials/create
+         (archive-config :user)
+         (archive-config :password))))
+      (.region software.amazon.awssdk.regions.Region/US_EAST_1)
+      (.build)))
+
+(defn- put-object [ archive-config file ]
+  (let [s3 (s3-client archive-config)]
+    (.putObject s3 (-> (software.amazon.awssdk.services.s3.model.PutObjectRequest/builder)
+                       (.bucket (archive-config :s3-bucket))
+                       (.key (.getName file))
+                       (.build))
+                (software.amazon.awssdk.core.sync.RequestBody/fromFile file))))
+
 ;;; backup
 
 (def date-format (java.text.SimpleDateFormat. "yyyyMMdd-hhmm"))
@@ -37,18 +55,27 @@
           (config/cval :app :name)
           (.format date-format (current-time))))
 
-(defn backup-database [ ]
-  (let [ output-path (get-backup-filename)]
-    (log/info "Backing database up to" output-path)
-    (sql-file/backup-to-file-online (current-db-connection) output-path)
-    (log/info "Database backup complete" output-path)))
+(defn- backup-database [ backup-file-name ]
+  (log/info "Backing database up to" backup-file-name)
+  (sql-file/backup-to-file-online (current-db-connection) backup-file-name)
+  (log/info "Database backup complete" backup-file-name))
+
+(defn- archive-backup-file [ backup-file-name ]
+  (when-let [ archive-config (config/cval :db :backup-archive )]
+    (let [ file (java.io.File. backup-file-name) ]
+      (log/info "Archiving backup file" backup-file-name)
+      (put-object archive-config file)
+      (log/info "File archive complete, deleting file" backup-file-name)
+      (.delete file))))
 
 (defn schedule-backup [ scheduler db-conn-pool ]
   (if-let [backup-path (config/cval :db :backup-path)]
     (do
       (log/info "Database backup configured with path: " backup-path)
       (scheduler/schedule-job scheduler :db-backup
-                              #(with-db-connection db-conn-pool
-                                 (backup-database))))
+                              #(let [ backup-path (get-backup-filename) ]
+                                 (with-db-connection db-conn-pool
+                                   (backup-database backup-path))
+                                 (archive-backup-file backup-path))))
     (log/warn "NO BACKUP PATH. AUTOMATIC BACKUP DISABLED!!!"))
   scheduler)
