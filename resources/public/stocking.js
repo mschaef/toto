c4226a9d75cd1130f8d1a76c9f71fe4a5d294b35
Mike Schaeffer <mike@mschaef.com>
2025-03-25 18:03:18 -0400

    Work to unify stocking.js and toto.js.

--- resources/public/stocking.js
+++ resources/public/stocking.js
@@ -242,71 +242,10 @@ function onDocumentClick(event) {
     }
 }
 
-//////// sidebar
-
-var sidebarVisible = false;
-
-function onToggleSidebar(evt) {
-    evt.preventDefault();
-
-    var menu = elemOptional('sidebar');
-
-    if (!menu || menu.contains(evt.target)) {
-        return;
-    }
-
-    doToggleSidebar(evt);
-}
-
-function doToggleSidebar(evt) {
-    evt.preventDefault();
-
-    sidebarVisible = !sidebarVisible;
-
-    updateSidebarVisibility(true);
-}
-
-function updateSidebarVisibility(animated) {
-    const menu = elemOptional('sidebar');
-
-    if (!menu) {
-        return;
-    }
-
-    menu.classList.toggle('animated', animated);
-    menu.classList.toggle('menu-visible', sidebarVisible);
-
-    if(sidebarVisible) {
-        document.addEventListener("mousedown", onToggleSidebar);
-    } else {
-        document.removeEventListener("mousedown", onToggleSidebar);
-    }
-}
-
-function setupSidebar() {
-    var toggleMenu = elemOptional('toggle-menu');
-
-    if (toggleMenu) {
-        toggleMenu.ontouchstart =  onToggleSidebar;
-        toggleMenu.onclick =  onToggleSidebar;
-    }
-
-    var closeMenu = elemOptional('close-menu');
-
-    if (closeMenu) {
-        closeMenu.ontouchstart = doToggleSidebar;
-        closeMenu.onclick = doToggleSidebar;
-    }
-};
-
 //////// todo list
 
-function setupEditableItems() {
-    foreach_elem('.toplevel-list .item-row.display', function(el) {
-        el.onclick = doubleTapFilter(null, function(obj) {
-            visitPage(el.getAttribute('edit-href'));
-        });
-    });
+function doSetToggle(el) {
+    visitPage(location.pathname + "?" + el.id  + "=" + el.checked);
 }
 
 function doUpdateItem(itemId, newDescription, action, thenUrl) {
@@ -321,25 +260,10 @@ function doUpdateItem(itemId, newDescription, action, thenUrl) {
     }).then(() => visitPage(thenUrl));
 }
 
-function doMoveItem(itemId, newListId) {
+function doSetItemOrdinal(itemId, newItemOrdinal) {
     var formData = new FormData();
 
-    formData.append("target-item", itemId);
-    formData.append("target-list", newListId);
-
-    server("/item/" + itemId + "/list", {
-        body: formData,
-        method: "POST",
-    }).then(doRefresh);
-}
-
-function doSetItemOrdinal(itemId, listId, newItemOrdinal, newItemPriority) {
-    var formData = new FormData();
-
-    formData.append("target-item", itemId);
-    formData.append("target-list", listId);
     formData.append("new-ordinal", newItemOrdinal);
-    formData.append("new-priority", newItemPriority);
 
     server("/item/" + itemId + "/ordinal", {
         body: formData,
@@ -348,7 +272,7 @@ function doSetItemOrdinal(itemId, listId, newItemOrdinal, newItemPriority) {
 }
 
 function setupItemDragging() {
-    foreach_elem(".item-drag-handle", function(el) {
+    foreach_elem(".drag-handle", function(el) {
         el.setAttribute('draggable', true);
 
         el.ondragstart = function(ev) {
@@ -371,96 +295,13 @@ function setupItemDragging() {
 
             var newListId = ev.currentTarget.getAttribute('listid');
             var newItemOrdinal = ev.currentTarget.getAttribute('ordinal');
-            var newItemPriority = ev.currentTarget.getAttribute('priority');
 
-            doSetItemOrdinal(itemId, newListId, newItemOrdinal, newItemPriority);
+            doSetItemOrdinal(itemId, newItemOrdinal);
         });
     });
-
-    foreach_elem('.list-list .list-drop-target', function(el) {
-        makeDropTarget(el, function(ev) {
-            ev.preventDefault();
-
-            var itemId = ev.dataTransfer.getData("text/plain");
-            var newListId = ev.currentTarget.getAttribute('listid');
-
-            doMoveItem(itemId, newListId);
-        });
-    });
-}
-
-
-function submitHighPriority() {
-    var form = elemBySelector('.new-item-form');
-    var priorityField = elemBySelector('.new-item-form #item-priority');
-
-    priorityField.value = "1";
-    form.submit();
-}
-
-function selectNavigate(id, direction) {
-    var s = elemById(id);
-
-    if (s.type !== 'select-one') {
-        return;
-    }
-
-    var newIndex = s.selectedIndex + direction;
-
-    if (newIndex < 0) {
-        newIndex = s.options.length - 1;
-    } else if (newIndex >= s.options.length) {
-        newIndex = 0;
-    }
-
-    s.selectedIndex = newIndex;
-}
-
-function onNewItemInputKeydown(event) {
-
-    if (event.ctrlKey) {
-        if (event.keyCode == 13) {
-            event.preventDefault();
-            event.stopPropagation();
-            submitHighPriority();
-        }
-
-    } else if (event.shiftKey) {
-        if (event.keyCode == 38) {
-            event.preventDefault();
-            event.stopPropagation();
-            selectNavigate('item-list-id', -1);
-        } else if (event.keyCode == 40) {
-            event.preventDefault();
-            event.stopPropagation();
-            selectNavigate('item-list-id', 1);
-        }
-    }
-}
-
-function onItemEditKeydown(event) {
-    var input = event.target;
-
-    if (event.keyCode == 13) {
-        event.preventDefault();
-        event.stopPropagation();
-
-        var action = "update";
-        if (event.ctrlKey) {
-            action = event.shiftKey ? "delete" : "complete";
-        }
-
-        doUpdateItem(input.getAttribute('item-id'),
-                     input.value,
-                     action,
-                     input.getAttribute('view-href'));
-    } else if (event.keyCode == 27) {
-        visitPage(input.getAttribute('view-href'));
-    }
 }
 
 function initTodoList() {
-    setupEditableItems();
     setupItemDragging();
 };
 
@@ -488,12 +329,127 @@ function initNewUser() {
     checkVerifyFields("email-addr", "email-addr-2", "E-mail addresses do not match.");
 };
 
+function onItemDescriptionKeydown(event) {
+    if (event.metaKey && event.keyCode == 13) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.target.form.submit();
+    }
+}
+
+function initAddStockingModal(modal) {
+    function doCheck() {
+        var toName = elemById("to-name").value.trim();
+        var fromName = elemById("from-name").value.trim();
+        var listDescription = elemById("list-description").value.trim();
+
+        elemById("submit-list").disabled = !toName || !fromName || !listDescription;
+    }
+
+    elemById("to-name").onkeyup = doCheck;
+    elemById("to-name").onchange = doCheck;
+    elemById("from-name").onkeyup = doCheck;
+    elemById("from-name").onchange = doCheck;
+    elemById("list-description").onkeyup = doCheck;
+    elemById("list-description").onchange = doCheck;
+}
+
+
+function pasteText(textEl, newText) {
+    var startPos = textEl.selectionStart;
+    var endPos = textEl.selectionEnd;
+
+    textEl.value = textEl.value.substring(0, startPos)
+        + newText
+        + textEl.value.substring(endPos, textEl.value.length);
+
+    textEl.selectionStart = startPos + newText.length;
+    textEl.selectionEnd = startPos + newText.length;
+}
+
+function onDescriptionPaste(event) {
+    const itemDescElem = elemById("item-description");
+    const item = event.clipboardData.items[0];
+
+    if (item.kind !== 'file') {
+        return;
+    }
+
+    const type = item.type;
+
+    var reader = new FileReader();
+    reader.onload = function(event){
+        server("/image", {
+            headers: {
+                'Content-Type': type
+            },
+            body: event.target.result,
+            method: "POST",
+        }).then(resp => resp.json())
+            .then(resp => {
+                pasteText(itemDescElem, "<img src=\"" + resp["url"] + "\"/>");
+            });
+    };
+    reader.readAsArrayBuffer(item.getAsFile());
+};
+
+function initItemModal(modal) {
+    function doCheck() {
+        var itemTitle = elemById("item-title").value.trim();
+        var itemDescription = elemById("item-description").value.trim();
+
+        elemById("submit-item").disabled = !itemTitle || !itemDescription;
+    }
+
+    var descriptionTimeout;
+
+    function doCheckDescription() {
+        let desc = elemById("item-description").value.trim();
+        let lengthIndicator = elemById("length-indicator");
+
+        lengthIndicator.textContent = desc.length;
+
+        if (!descriptionTimeout) {
+            descriptionTimeout = setTimeout(function() {
+                descriptionTimeout = null;
+
+                let desc = elemById("item-description").value.trim();
+
+                server("/format-item-body", {
+                    body: desc,
+                    method: "POST",
+                }).then((response) => {
+                    response.text().then(function(html) {
+                        elemById("item-preview").innerHTML = html;
+                    });
+                });
+
+            }, 1000);
+        }
+
+        doCheck();
+    }
+
+    const itemTitleElem = elemById("item-title");
+    const itemDescElem = elemById("item-description");
+
+    itemTitleElem.onkeyup = doCheck;
+    itemTitleElem.onchange = doCheck;
+
+    itemDescElem.onkeyup = doCheckDescription;
+    itemDescElem.onchange = doCheckDescription;
+
+    itemDescElem.onpaste = onDescriptionPaste;
+}
+
 const initFns = {
     'todo-list': initTodoList,
     'init-new-user': initNewUser
 };
 
 const initModalFns = {
+    'item-modal': initItemModal,
+    'add-stocking-modal': initAddStockingModal
 };
 
 function runPageInitScript() {
@@ -516,6 +472,7 @@ function runPageInitScript() {
     }
 };
 
+
 window.savedScrolls = {};
 
 function saveScrolls() {
@@ -528,7 +485,13 @@ function saveScrolls() {
     });
 }
 
-function restoreScrolls() {
+function restoreScrollAndAutofocusStates(_) {
+    const autofocusedElements = document.querySelectorAll('input[autofocus]');
+
+    if (autofocusedElements.length) {
+        autofocusedElements[0].focus();
+    }
+
     for(const id in window.savedScrolls) {
         const elem = elemOptionalById(id);
 
@@ -536,54 +499,6 @@ function restoreScrolls() {
             elem.scrollTop = window.savedScrolls[elem.id];
         }
     }
-}
-
-function updateScrollState(elem) {
-    elem.classList.toggle('scrolled', elem.scrollTop > 0);
-    elem.classList.toggle('additional-content', elem.scrollHeight - elem.scrollTop > elem.clientHeight);
-}
-
-function setupScrollListeners() {
-    foreach_elem(".scroll-column .scrollable", function(elem) {
-        updateScrollState(elem);
-
-        elem.addEventListener('scroll', (event) => {
-            updateScrollState(event.target);
-        });
-    });
-}
-
-
-function isDisplayingAllLists() {
-    let params = (new URL(document.location)).searchParams;
-    let minListPriority = params.get("min-list-priority");
-
-    return minListPriority && minListPriority == "-1";
-}
-
-var prevDisplaysAllLists = isDisplayingAllLists();
-
-function displaySidebarOnChangingListQuery() {
-    const displaysAllLists = isDisplayingAllLists();
-
-    sidebarVisible = displaysAllLists != prevDisplaysAllLists;
-    prevDisplaysAllLists = displaysAllLists;
-}
-
-function restoreScrollAndAutofocusStates(_) {
-    setupSidebar();
-
-    const autofocusedElements = document.querySelectorAll('input[autofocus]');
-
-    if (autofocusedElements.length) {
-        autofocusedElements[0].focus();
-    }
-
-    displaySidebarOnChangingListQuery();
-
-    restoreScrolls();
-    updateSidebarVisibility(false);
-    setupScrollListeners();
 };
 
 // Copy
@@ -601,22 +516,17 @@ function doCopy(event) {
 
 // startup
 
-document.addEventListener('DOMContentLoaded', function() {
-    setupSidebar();
-    setupScrollListeners();
-}, false);
-
 document.addEventListener("turbo:load", runPageInitScript);
 document.addEventListener("turbo:before-visit", saveScrolls);
 document.addEventListener("turbo:render", restoreScrollAndAutofocusStates);
 document.addEventListener("keydown", onDocumentKeydown);
 document.addEventListener("click", onDocumentClick);
 
+// exports
+
 window._toto = {
     doPost,
     doCopy,
-    onItemEditKeydown,
-    onNewItemInputKeydown,
-    server,
-    submitHighPriority
+    doSetToggle,
+    onItemDescriptionKeydown
 };
