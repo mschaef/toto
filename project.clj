1531160fcf9f05c74d466d2fa9651b112f3bf6d4
Mike Schaeffer <mike@mschaef.com>
2025-05-17 14:24:38 -0400

    Update most dependencies.

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :license { :name "Copyright East Coast Toolworks (c) 2012-2025"}
 
   :dependencies [[org.clojure/clojure "1.12.0"]
-                 [commons-io "2.18.0"]
-                 [com.mschaef/sql-file "0.4.12"]
+                 [commons-io "2.19.0"]
+                 [com.mschaef/sql-file "0.4.13"]
                  [cprop "0.1.20"]
                  [yesql "0.6.0-alpha1"]
-                 [org.clojure/data.json "2.5.0"]
+                 [org.clojure/data.json "2.5.1"]
                  [clj-http "3.13.0"]
                  [hiccup "1.0.5"]
                  [ring/ring-jetty-adapter "1.13.0"]
@@ -16,11 +16,11 @@
                  [slester/ring-browser-caching "0.1.1"]
                  [com.cemerick/friend "0.2.3"]
                  [compojure "1.7.1"]
-                 [joda-time/joda-time "2.13.0"]
+                 [joda-time/joda-time "2.14.0"]
                  [com.draines/postal "2.0.5"]
                  [it.sauronsoftware.cron4j/cron4j "2.2.5"]
                  [com.mschaef/playbook "0.1.5"]
-                 [software.amazon.awssdk/s3  "2.29.19"]]
+                 [software.amazon.awssdk/s3  "2.31.45"]]
 
   :plugins [[lein-tar "3.3.0"]
             [dev.weavejester/lein-cljfmt "0.13.0"]]
